257825d1de8e42b7d5bdf5b24f314eee4f4810ad
Divya Sivasankaran <div1090@gmail.com>
2017-04-19 19:03:20 +0800

    adding a general lda face verifier that is trained on all types of data

--- Experts/LDA/FaceVerifier.py
+++ Experts/LDA/FaceVerifier.py
@@ -21,7 +21,7 @@ from scipy import spatial
 
 K=30
 R=90
-alpha = 0.1
+alpha = 0.6
 np.set_printoptions(threshold=np.nan)
 
 def reduceDimension(faces_train,id_train,r):
@@ -67,11 +67,9 @@ def generateTemplate_LDA(faces_train,id_train):
         z[classLabels[i]] = np.divide(z[classLabels[i]],z_count[classLabels[i]])
 
     return np.dot(np.transpose(W_f),np.transpose(W_1)),m,z
-def getfiles(Dir,mode,fold,context):
+def getfiles(data,mode,fold,context):
     lst = []
-    file = open("KFold_Split.csv","r")
-    lines = file.readlines()
-    for line in lines:
+    for line in data:
         line = line.strip()
         ID,f,c,k,train,claim = line.split(",")
         
@@ -87,16 +85,21 @@ def getfiles(Dir,mode,fold,context):
             if int(k)!=fold:
                 continue
             lst.append(line)
-    file.close()
     return lst
 def main():
     currDir = "C:/Users/div_1/Desktop/Scripts/LDA/Dataset_57"
     #currDir = "C:\Users\e0013178\Google Drive\Study\Course Material\Multimedia Analysis\hw2"
     #Training classifiers
     #context pose
-    for k_fold in range(3,4):
+    c = dict()
+    current_context_count = 1
+    infile = open("KFold_Split.csv","r")
+    data = infile.readlines()  
+    data = np.random.permutation(data)
+    infile.close()
+    for k_fold in range(1,4):
         print(k_fold)
-        lst = getfiles(currDir,"train",k_fold,["0"])
+        lst = getfiles(data,"train",k_fold,["0"])
         print("Files: ",len(lst))
         faces_train,id_train,context_train = ff.read_faces(currDir,lst)
         Plda_w,Plda_m,Plda_feature = generateTemplate_LDA(faces_train,id_train)
@@ -104,13 +107,21 @@ def main():
         print("mean",Plda_m.shape)
         
         #context expression
-        lst = getfiles(currDir,"train",k_fold,["1"])
+        lst = getfiles(data,"train",k_fold,["1"])
         print("Files: ",len(lst))
         faces_train,id_train,context_train = ff.read_faces(currDir,lst)
         Elda_w,Elda_m,Elda_feature = generateTemplate_LDA(faces_train,id_train)
         print("LDA_f",Elda_feature[2].shape)
+         
+        #train on all data - trying to be best at everything!
+        lst = getfiles(data,"train",k_fold,["0","1"])
+        print("Files: ",len(lst))
+        faces_train,id_train,context_train = ff.read_faces(currDir,lst)
+        Glda_w,Glda_m,Glda_feature = generateTemplate_LDA(faces_train,id_train)
+        print("LDA_f",Elda_feature[2].shape)
         
-        lst = getfiles(currDir,"test",k_fold,["0","1"])
+        
+        lst = getfiles(data,"test",k_fold,["0","1"])
         print("testing list",len(lst))
         faces_test,id_test,context_test = ff.read_faces(currDir,lst)
         #Testing classifiers
@@ -121,10 +132,26 @@ def main():
             l = lst[i]
             Py_lda = np.dot(Plda_w,f-Plda_m)
             Ey_lda = np.dot(Elda_w,f-Elda_m)
+            Gy_lda = np.dot(Glda_w,f-Glda_m)
             claimID = l.split(",")[-1]
+            c_id,c_session,c_exp,c_pose,c_illm = l.split(",")[1].split("_")
+            if str(c_exp)+str(c_pose) not in c.keys():
+                c[ str(c_exp)+str(c_pose)] = current_context_count
+                current_context_count += 1
             pDist = spatial.distance.cosine(Plda_feature[int(claimID)], Py_lda)
             eDist = spatial.distance.cosine(Elda_feature[int(claimID)], Ey_lda)
-            fout.write((",").join([str(id_test[i]),claimID,str(context_test[i]),str(pDist),str(eDist)]))
+            gDist = spatial.distance.cosine(Glda_feature[int(claimID)], Gy_lda)
+            p_d = 0
+            e_d = 0
+            g_d = 0
+            true_d = int(claimID == c_id)
+            if pDist <= alpha:
+                p_d = 1
+            if eDist <= alpha:
+                e_d = 1
+            if gDist <= alpha:
+                g_d = 1
+            fout.write((",").join([str(id_test[i]),str(claimID),str(context_test[i]),str(pDist),str(eDist),str(gDist),str(c[ str(c_exp)+str(c_pose)]),str(p_d),str(e_d),str(g_d),str(true_d)]))
             fout.write("\n")
         fout.close()
 main()
